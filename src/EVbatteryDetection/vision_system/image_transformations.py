56f02ae6fb3973e3b72a83d29c46db1ad7f2738a
Claudio <cima.claudio@gmail.com>
2024-02-26 01:35:10 +0100

    update transform_points fn input type verification

--- src/EVbatteryDetection/vision_system/image_transformations.py
+++ src/EVbatteryDetection/vision_system/image_transformations.py
@@ -1,11 +1,14 @@
 import cv2
+import numpy as np
 
 class HomographyEstimator:
     def __init__(self, src_pts, dst_pts, repr_thr=5.0) -> None:
         self.H = None
-        H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, ransacReprojThreshold=repr_thr)
+        H, inl = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, ransacReprojThreshold=repr_thr)
         self.H = H
-        self.mask = mask
+        self.inliers = inl
 
     def transform_points(self, points):
+        points = np.array(points, np.float32)
+        points = points.reshape((-1, 1, 2))
         return cv2.perspectiveTransform(points, self.H)
