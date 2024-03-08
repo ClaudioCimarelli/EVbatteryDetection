f8e3b749b68d280776c8869f725dc09fb9af11e6
Claudio <cima.claudio@gmail.com>
2024-03-08 14:44:26 +0100

    update main function

--- src/main.py
+++ src/main.py
@@ -11,11 +11,12 @@ import numpy as np
 def main():
     # Load templates
     templates_dir = 'data/images/templates'
-    matcher = TemplateDetector(*process_annotations(templates_dir), resize_factor=.5)
+    templ_imgs_p, templ_labels = process_annotations(templates_dir)
+    matcher = TemplateDetector(templ_imgs_p, templ_labels, resize_factor=.3)
     all_images_dir = 'data/images/all'
-    all_imgs, _ = process_annotations(all_images_dir)
+    all_imgs_p, _ = process_annotations(all_images_dir)
     times = []
-    for p in all_imgs.values():
+    for p in all_imgs_p.values():
         t1_start = perf_counter()
         det_res: DetectionResult = matcher.one_templ_detection(p)
         times.append(perf_counter() - t1_start)
@@ -23,12 +24,12 @@ def main():
             print(f"Detection not found in time: {times[-1]:.2f} secs")
             continue
         print(f"Detection found at Template ID{det_res.template_id} in time: {times[-1]:.2f} secs!")
-        # test_img = load_image(p, color=False)
-        # templ_img = cv2.cvtColor(det_res.templ_img, cv2.COLOR_RGB2GRAY)
-        # draw_matches(templ_img, det_res.match_res.kps1, test_img, det_res.match_res.kps2, det_res.match_res.matches, True)
+        test_img = cv2.resize(load_image(p, color=False), det_res.img_shape, interpolation=cv2.INTER_AREA)
+        templ_img = cv2.resize(load_image(templ_imgs_p[det_res.template_id], color=False), det_res.img_shape, interpolation=cv2.INTER_AREA)
+        draw_matches(templ_img, det_res.match_res.kps1, test_img, det_res.match_res.kps2, det_res.match_res.matches, True)
 
-        test_img_color = cv2.resize(load_image(p, color=False), det_res.img_shape, interpolation=cv2.INTER_AREA)
-        overlay_mask_with_border(test_img_color, mask=det_res.mask, polygon=det_res.polygon, draw_now=True)
+
+        overlay_mask_with_border(test_img, mask=det_res.mask, polygon=det_res.polygon, draw_now=True)
 
     times = np.array(times)
     print(f"Avg time per detection:{times.mean():.2f} secs")
